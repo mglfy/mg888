972490bbbc4053a2f4edcc8e185d38b0364f401e
mglfy <160197347+mglfy@users.noreply.github.com>
2024-02-16 11:03:25 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          GPT6.2
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+         ai全能助手.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
